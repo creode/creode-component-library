d0fa6f29f5848a05b91a0bf313673956f34be98f
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-08-22 09:58:19 +0100

    Update components to use HTML and JS instead of Vue.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-import header from "./src/components/header/index.js";
-import banner from "./src/components/banner/index.js";
-import grid from "./src/components/grid/index.js";
-import teaser from "./src/components/teaser/index.js";
-import accordion from "./src/components/accordion/index.js";
-import tabbed_content from './src/components/tabbed-content/index.js';
-import modal from './src/components/modal/index.js';
+import Header from "./src/components/header/index.js";
 
 export {
-  header,
-  banner,
-  grid,
-  teaser,
-  accordion,
-  tabbed_content,
-  modal
+  Header,
 };
--- src/components/modal/index.js
+++
@@ -1,8 +0,0 @@
-import ComponentBundle from '../../classes/component-bundle.js'
-import main from './main.vue';
-
-var bundle = new ComponentBundle('modal');
-
-bundle.addMainComponent(main);
-
-export default bundle;
